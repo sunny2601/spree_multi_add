9167d4777e681e15a037e2efe2d36ba0fde244a5
Patrik Johansson <patrik.j.johansson@kentor.se>
2014-03-20 09:21:19 +0100

    Reusing the existing populate action for orders
    There is no need to a create custom action for OrdersController if the
    form data is in correct format.
    
    The populate-methods can consume variants with this format: {:variants
    => {variant_id => quantity}}

--- app/overrides/views_decorator.rb
+++ app/overrides/views_decorator.rb
@@ -3,13 +3,10 @@
 Deface::Override.new(:virtual_path => "spree/products/_cart_form",
                      :name => "add_multi_add",
                      :replace => "div#product-variants",
-                     :partial => "spree/products/cart_with_multi_add",
-                     :disabled => false)
+                     :partial => "spree/products/cart_with_multi_add")
 
 
 # Override for only showing the "Add to cart"-button below the multi-add feature.
 Deface::Override.new(:virtual_path => "spree/products/_cart_form",
                      :name => "add_multi_add_button",
-                     :remove => "div.add-to-cart erb[loud]:contains('number_field_tag')",
-                     :disabled => false)
-
+                     :remove => "div.add-to-cart erb[loud]:contains('number_field_tag')")
